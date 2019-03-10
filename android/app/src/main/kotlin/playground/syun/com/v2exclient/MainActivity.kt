6acd81e72de6476fd26d637b4413bbb3dfcd4853
syun <517897442@qq.com>
2019-03-10 22:10:22 +0800

    init project

---
+++ android/app/src/main/kotlin/playground/syun/com/v2exclient/MainActivity.kt
@@ -0,0 +1,13 @@
+package playground.syun.com.v2exclient
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
